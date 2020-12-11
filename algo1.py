c0ef310511a042d654f544a4fdd9578335380f00
reuyi <46411171+reuyi@users.noreply.github.com>
2020-12-11 16:37:07 +0900

    Create algo1.py
    Validate Subsequence

---
+++ algo1.py
@@ -0,0 +1,8 @@
+def isValidSubsequence(array, sequence):
+  arrayIndex = 0
+  sequenceIndex = 0
+  while arrayIndex < len(array) and sequenceIndex < len(sequence):
+    if array[arrIndex] == sequence[sequenceIndex]:
+      sequenceIndex += 1
+    arrayIndex +=1
+  return sequenceIndex == len(sequence)
