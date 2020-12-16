8b21a60a67a835bba74c654767fa5df3f73d819e
reuyi <46411171+reuyi@users.noreply.github.com>
2020-12-16 20:13:39 +0900

    Update is_monotonic.py

--- is_monotonic.py
+++ is_monotonic.py
@@ -16,3 +16,13 @@ def breaks_direction(direction, previous_int, current_int):
   if direction > 0:
     return difference < 0:
   return difference > 0
+
+def is_monotonic(array):
+  is_non_decreasing = True
+  is_non_increasing = True
+  for i in range(1, len(array)):
+    if array[i] < array[i-1]:
+      is_non_decreasing = False
+    if array[i] > array[i-1]:
+      is_non_increasing = False 
+  return is_non_decreasing or is_non_increasing
